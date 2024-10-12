dd8781da6159e7cd528ecefde0f6d0552b5ac5ce
Franslethabogithub3738 <130747361+Franslethabogithub3738@users.noreply.github.com>
2024-10-12 12:17:19 +0200

    Add files via upload

---
+++ HotelDiscount.java
@@ -0,0 +1,81 @@
+import java.util.Scanner; //Scanner class is imported from java.util package
+import java.text.DecimalFormat; //DecimalFormat class is imported from java.text package
+
+//@Author Lethabo Matsi
+
+public class HotelDiscount
+{//Start class
+	public static void main(String[] args)
+	{//Start main method
+		
+		//Instantiation of objects
+		Scanner input = new Scanner(System.in);
+		DecimalFormat formatter = new DecimalFormat("R,##0.00");
+		
+		//Declare variables
+		double rHotelBill, rDiscount = 0, rTotalDiscount = 0;
+		int iNumActivities;
+		int iCounter = 0;
+		int iOption = 0;
+		
+		//Declare constants
+		final double ZIP_LINING = 0.1;
+		final double HORSE_RIDING = 0.125;
+		final double SITE_SEEING = 0.0;
+		final double SPEED_BOAT = 0.1;
+		
+		//Input
+		System.out.print("How much is your original hotel bill: R");
+		rHotelBill = input.nextDouble();
+		System.out.println("");
+		System.out.print("");
+		System.out.print("How many activities did you do?: ");
+		iNumActivities = input.nextInt();
+		System.out.println("");
+		
+		while ( iNumActivities != iCounter )
+		{//Begin while
+			iCounter++;
+			System.out.println("Activities		Discount");
+			System.out.println("1. Zip-lining		10%");
+			System.out.println("2. Horse riding		12.5%");
+			System.out.println("3. Site seeing		0%");
+			System.out.println("4. Speedboat		10%");
+			System.out.print("Pick your activity no "+iCounter+" < options 1 to 4>: ");
+			iOption = input.nextInt();
+			
+			switch ( iOption )
+			{//Begin switch
+				case 1:
+					rDiscount = rHotelBill*ZIP_LINING;
+					rTotalDiscount += rDiscount;
+				break;
+				
+				case 2:
+					rDiscount = rHotelBill*HORSE_RIDING;
+					rTotalDiscount += rDiscount;
+				break;
+				
+				case 3:
+					rDiscount = rHotelBill*SITE_SEEING;
+					rTotalDiscount += rDiscount;
+				break;
+				
+				case 4:
+					rDiscount = rHotelBill * SPEED_BOAT;
+					rTotalDiscount += rDiscount;
+				break;
+			}//End switch
+			
+			//Output
+			System.out.println("So far, your discount is :"+formatter.format(rTotalDiscount));
+			System.out.println("");
+		}//End while
+		
+		//Final Discount
+		System.out.println("Your total discount is: "+formatter.format(rTotalDiscount));
+		rHotelBill -= rTotalDiscount;
+		System.out.println("After discount, your bill will be "+formatter.format(rHotelBill));
+		
+	}//End main method
+}//End class
